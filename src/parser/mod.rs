727d171237f0c80d0da77f51471c26670efe2083
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-09-01 15:05:22 -0300

    feat(parser): add support for logical AND/OR operators
    - Add new `or()` method handling logical OR expressions
    - Add new `and()` method handling logical AND expressions
    - Update `assignment()` to parse `or()` instead of `equality()`
    - Logical operators follow standard precedence: OR lower than AND
    - Both operators are left-associative

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -147,7 +147,7 @@ impl Parser {
     }
 
     fn assignment(&mut self) -> Result<Expression> {
-        let expr = self.equality()?;
+        let expr = self.or()?;
 
         if self.match_tokens(&[TokenType::Equal]) {
             let equals = self.previous()?;
@@ -167,6 +167,38 @@ impl Parser {
                     }
                     .into());
                 }
+            }
+        }
+
+        Ok(expr)
+    }
+
+    fn or(&mut self) -> Result<Expression> {
+        let mut expr = self.and()?;
+
+        while self.match_tokens(&[TokenType::Or]) {
+            let operator = self.previous()?;
+            let right = self.and()?;
+            expr = Expression::Logical {
+                left: Box::new(expr),
+                operator,
+                right: Box::new(right),
+            }
+        }
+
+        Ok(expr)
+    }
+
+    fn and(&mut self) -> Result<Expression> {
+        let mut expr = self.equality()?;
+
+        while self.match_tokens(&[TokenType::And]) {
+            let operator = self.previous()?;
+            let right = self.equality()?;
+            expr = Expression::Logical {
+                left: Box::new(expr),
+                operator,
+                right: Box::new(right),
             }
         }
 
